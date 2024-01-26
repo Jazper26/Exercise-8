f020fddfd137ca757705e5c0bb6384d551654dae
Jazper26 <jazperhaapin89Gmail.com>
2024-01-26 21:05:53 +0800

    This one iterate each row to print the numbers within and also print the current number

--- Exercise 8.py
+++ Exercise 8.py
@@ -3,4 +3,9 @@ ows = 5
 
 # Iterate through each row
 for i in range(1, rows + 1):
+    # Iterate within each row to print the numbers
+    for f in range(i):
+        
+        print(i, end=" ")  # Print the current row number
+        
     
